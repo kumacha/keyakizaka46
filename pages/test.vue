2e1dd969b9558c132ae2c2d32a8cb330a75a2aff
kumatyan-0919 <akakuma.yuto.19@shizoka.ac.jp>
2021-01-18 04:01:17 +0900

    galleryの素材集めた

--- pages/test.vue
+++ pages/test.vue
@@ -8,7 +8,7 @@
       <v-lazy v-model="isActive" :options="{
           threshold: 1.0
         }" min-height="200" transition="fade-transition">
-        <v-card class="mx-auto" max-width="336">
+        <v-card class="mx-auto" max-width="336" hover="true" loading="true">
           <v-img src="https://firebasestorage.googleapis.com/v0/b/history-of-keyakizaka46.appspot.com/o/1st-pic%2F1st-rika.jpg?alt=media&token=aa2ffa20-8dc8-40e6-89b7-ce3a59491d41"></v-img>
           <v-card-title>渡辺梨加</v-card-title>
           <v-card-text>
