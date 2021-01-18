fe09c99af35ea3fd78598df4b720d6c85f6c3c28
kumatyan-0919 <akakuma.yuto.19@shizoka.ac.jp>
2021-01-18 14:54:14 +0900

    三人分の進捗合体

--- pages/member.vue
+++ pages/member.vue
@@ -1,9 +1,26 @@
 <template>
+<v-cpntent>
+  <v-container>
+    <v-row>
+        <Member/>
 
+        <Member2/>
+    </v-row>
+  </v-container>
+</v-cpntent>
+</template>
+
+<script>
+import Member from '~/components/Member.vue'
+import Member2 from '~/components/Member2.vue'
+export default {
+components:{
+  Member,
+  Member2
+}
+}
 </script>
 
 <style>
-.member-card :hover{
-opacity: 70%;
-}
+
 </style>
