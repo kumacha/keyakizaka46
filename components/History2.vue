ebdc7b9c38f32f1a01faa2469328c5519fc7a576
kumatyan-0919 <akakuma.yuto.19@shizoka.ac.jp>
2021-01-24 20:10:20 +0900

    余計な背景消してHISTORY少し入れた

---
+++ components/History2.vue
@@ -0,0 +1,50 @@
+<template>
+<v-content>
+<div class="histopic2">
+      <div class="histopic2text1"><p>5年と1か月と24日</p></div>
+        <div class="histopic2text2"><p>1882日の欅坂46のすべて</p></div>
+        </div>
+        <div class="historyline"></div>
+</v-content>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.histopic2{
+  width: 100vw;
+    height: 100vh;
+    position: relative;
+    background-size: cover;
+    background-position: center center;
+    background-color: transparent;
+}
+.histopic2text1 p {
+  text-align: center;
+  font-size: 50px;
+  position: absolute;
+  left: 50%;
+  top: 55%;
+  -ms-transform: translate(-50%, -55%);
+  -webkit-transform: translate(-50%, -55%);
+  transform: translate(-50%, -55%);
+  margin: 0;
+  padding: 0;
+}
+.histopic2text2 p {
+  text-align: center;
+  font-size: 50px;
+  position: absolute;
+  left: 50%;
+  top: 65%;
+  -ms-transform: translate(-50%, -65%);
+  -webkit-transform: translate(-50%, -65%);
+  transform: translate(-50%, -65%);
+  margin: 0;
+  padding: 0;
+}
+</style>
